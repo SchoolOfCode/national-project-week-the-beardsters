bdd7aa4bedc1758c8c751dcc1dcbc5cd0c8565bf
marian.eq <mariano.eq@gmail.com>
2022-01-18 14:21:19 -0300

    Routes done

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,22 @@ import { getChallenge, getChallengesByType } from "../models/index";
 //GET RANDOM CHALLENGE
 
 router.get("/", async (req, res) => {
-  const title = req.query.title;
+  const randomChallenge = await getChallenge();
+  res.json({
+    success: true,
+    message: "Random challenge found",
+    payload: randomChallenge,
+  });
+});
+
+//GET CHALLENGE BY TYPE
+
+router.get("/:type", async (req, res) => {
+  const { type } = req.params.type;
+  const requestedChallenge = await getChallengesByType(type);
+  res.json({
+    success: true,
+    message: `Challenge found ${type}`,
+    payload: requestedChallenge,
+  });
 });
