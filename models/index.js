1e32245d0aed0e056a5fafb968eebc407bce5c2e
Antonio Riccelli <anton.riccelli@gmail.com>
2022-01-19 09:38:56 +0000

    fixed the random integer generating function

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 import query from "../db/index.js";
 
 export async function getChallenge() {
-  const randomChallenge = Math.floor(Math.random() * 2);
+  const randomChallenge = Math.floor(Math.random() * 2) + 1;
+  console.log(randomChallenge);
   const challengesData = await query(
     `SELECT * FROM challenges JOIN answers on challenges.id = answers.id WHERE challenges.id = $1;`, [
       randomChallenge
