a374e304302a6209aaf34431059ea723d7a16306
Mark Young <markyoung76@me.com>
2022-01-20 21:03:33 +0000

    getChallenge model adjusted to allow questions and answers to match when rendered to the frontend and database table object data corrected

--- models/index.js
+++ models/index.js
@@ -4,9 +4,8 @@ export async function getChallenge() {
   const randomChallenge = Math.floor(Math.random() * 2) + 1;
   console.log(randomChallenge);
   const challengesData = await query(
-    `SELECT * FROM challenges JOIN answers on challenges.id = answers.id WHERE challenges.id = $1;`, [
-      randomChallenge
-    ]
+    `SELECT * FROM challenges, answers WHERE challenges.id = $1 AND answers.id = $1;`,
+    [randomChallenge]
   );
   return challengesData.rows;
 }
@@ -18,4 +17,3 @@ export async function getChallengesByType(type) {
   );
   return challengesType.rows;
 }
-
